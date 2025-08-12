ed1b3fff40d554d51de823de857c96a2f42fcf09
Ngô Trường An <anngo270398@gmail.com>
2025-08-12 15:49:37 +0700

    train

--- train.py
+++ train.py
@@ -2,32 +2,23 @@
 from ultralytics import YOLO
 
 if __name__ == '__main__':
-    # model = YOLO(r'ultralytics/cfg/models/v8/yolov8-p2-test.yaml')
-    # model = YOLO(r'ultralytics/cfg/models/11/yolo11-p2-bifpn-mhsa.yaml')
-    # model = YOLO(r'ultralytics/cfg/models/11/yolo11-SAHI-AKConv.yaml')
-    # model = YOLO(r'ultralytics/cfg/models/11/yolo11-AKConv.yaml')
-    # model = YOLO(r'ultralytics/cfg/models/11/yolo11-C3k2-IDC.yaml')
-    # model = YOLO(r'ultralytics/cfg/models/11/yolo11-SAHI-MHSA.yaml')
-    model = YOLO(r'ultralytics/cfg/models/11/yolo11-test-09.yaml')
-    # model = YOLO(r'ultralytics/cfg/models/11/yolo11-IDC.yaml')
-    # model = YOLO(r'ultralytics/cfg/models/11/yolo11-DynamicConv2D.yaml')
-    # model = YOLO(r'ultralytics/cfg/models/11/yolo11-GAM.yaml')
-    # model = YOLO(r'ultralytics/cfg/models/11/yolo11-SAHI-CA.yaml')
-    # model = YOLO(r'ultralytics/cfg/models/11/yolo11-C3k2-IDC.yaml')
-    # model = YOLO(r'ultralytics/cfg/models/v12/yolov12-A2C2f_SimAM.yaml')
 
+    model = YOLO(r'ultralytics/cfg/models/11/yolo11-p2-bifpn-gam.yaml')
 
     model.train(
         data=r'data/coco_dataset/data.yaml',
-        cache=False,
         imgsz=640,
-        epochs= 2,
-        single_cls=False,
-        # batch=2,
-        # close_mosaic=0,
-        # worker=0,
-        device='cpu',
-        amp=False,
+        batch=32,                         # Ổn định hơn, ít dao động mAP
+        epochs=150,
+        cache=False,
+        amp=False,                        # FP32 cho độ chính xác cao nhất
+        optimizer='SGD',
+        patience=30,
+        save_period=10,
+        seed=42,
         project='runs/train',
-        name='exp'
+        name='exp',
+        workers=16,
+        device='cpu',
+        val=True
     )
