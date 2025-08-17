1388323c30fd884fe78bc15e7bc62408f02c1cdc
Ngô Trường An <anngo270398@gmail.com>
2025-08-17 19:44:02 +0700

    test

--- train.py
+++ train.py
@@ -3,7 +3,7 @@ from ultralytics import YOLO
 
 if __name__ == '__main__':
 
-    model = YOLO(r'ultralytics/cfg/models/11/yolo11-test-07.yaml')
+    model = YOLO(r'ultralytics/cfg/models/11/yolo11-test-08.yaml')
     # model = YOLO(r'ultralytics/cfg/models/11/yolo11-p2-bifpn-mhsa.yaml')
 
     model.train(
