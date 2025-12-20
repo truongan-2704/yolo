d4ab47b6c941dd4a8ae415a30fa4bb6fe9828b77
truongan-2704 <anngo270398@gmail.com>
2025-12-20 21:02:51 +0700

    Update new

--- ultralytics/nn/modules/BiFPN.py
+++ ultralytics/nn/modules/BiFPN.py
@@ -14,22 +14,6 @@ def autopad(k, p=None, d=1):
     return p
 
 
-class Conv(nn.Module):
-    default_act = nn.SiLU()  # default activation
-
-    def __init__(self, c1, c2, k=1, s=1, p=None, g=1, d=1, act=True):
-        super().__init__()
-        self.conv = nn.Conv2d(c1, c2, k, s, autopad(k, p, d), groups=g, dilation=d, bias=False)
-        self.bn = nn.BatchNorm2d(c2)
-        self.act = self.default_act if act is True else act if isinstance(act, nn.Module) else nn.Identity()
-
-    def forward(self, x):
-        return self.act(self.bn(self.conv(x)))
-
-    def forward_fuse(self, x):
-        return self.act(self.conv(x))
-
-
 class BiFPN_Concat(nn.Module):
     def __init__(self, c1, c2):
         super(BiFPN_Concat, self).__init__()
@@ -143,34 +127,24 @@ class BiFPN_Concat(nn.Module):
 
 class BiFPN(nn.Module):
     def __init__(self, ch, c2):
-        # ch: list số kênh đầu vào
-        # c2: số kênh đầu ra mong muốn
         super().__init__()
 
         self.w = nn.Parameter(torch.ones(len(ch), dtype=torch.float32), requires_grad=True)
         self.epsilon = 1e-4
-
-        # --- SỬA CHỮA QUAN TRỌNG ---
-        # Thay nn.Conv2d bằng Conv của Ultralytics (Có Batch Normalization + SiLU)
         self.convs = nn.ModuleList()
+
         for c_in in ch:
-            # Conv(input, output, kernel, stride)
-            # act=True (Mặc định) nghĩa là có SiLU
             self.convs.append(Conv(c_in, c2, 1, 1))
 
         self.act = nn.SiLU()
 
     def forward(self, x):
-        # 1. Trọng số (Fast Normalized Fusion)
+
         w = self.w
         weight = w / (torch.sum(w, dim=0) + self.epsilon)
-
-        # 2. Tính toán
         out = 0
         for i in range(len(x)):
-            # Bây giờ self.convs[i] đã có BatchNorm, mạng sẽ học tốt hơn nhiều
             out += weight[i] * self.convs[i](x[i])
-
         return self.act(out)
 
 class MultiHeadSelfAttention(nn.Module):
--- ultralytics/nn/tasks.py
+++ ultralytics/nn/tasks.py
@@ -1079,7 +1079,6 @@ def parse_model(d, ch, verbose=True):  # model_dict, input_channels(3)
             c1, c2 = ch[f], args[0]
             if c2 != nc:  # if c2 not equal to number of classes (i.e. for Classify() output)
                 c2 = make_divisible(min(c2, max_channels) * width, 8)
-
             args = [c1, c2, *args[1:]]
         elif m is Concat:
             c2 = sum(ch[x] for x in f)
